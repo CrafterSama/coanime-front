6cf8c9f56c7b298a6c0ebe078138fd2d23990b93
Julmer Olivero <julmer.olivero@meru.com>
2022-09-01 14:35:26 -0400

    Added Gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 import NextNProgress from 'nextjs-progressbar';
+
+import * as gtag from '../lib/gtag';
 
 import '@/styles/DateTimePicker.css';
 import '@/styles/Calendar.css';
@@ -9,6 +14,18 @@ import '@/styles/Clock.css';
 import '@/styles/app.css';
 
 const App = ({ Component, pageProps: { ...pageProps } }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -31,6 +48,25 @@ const App = ({ Component, pageProps: { ...pageProps } }) => {
             style: {
               marginTop: '30px',
             },
+          }}
+        />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
           }}
         />
         <Component {...pageProps} />
